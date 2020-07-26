dd3f6f5681ce0a72bb4d6ccc46d9533272f6d958
Sergio Barbosa <s.barbosa@chargebacks911.com>
2020-07-26 15:25:19 -0400

    [update] Added first service and Entity Owner.

---
+++ src/Entity/Owner.ts
@@ -0,0 +1,11 @@
+export class Owner {
+    name: string;
+    username: string;
+    email: string;
+
+    constructor(name: string, username: string, email: string) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+}
